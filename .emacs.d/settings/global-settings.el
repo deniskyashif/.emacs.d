0ee68cb49205103a3fb7d1cccac894bd713d0670
denis <denis.kyashif@gmail.com>
2015-05-12 23:55:40 +0300

    updated global-settings.el

--- .emacs.d/settings/global-settings.el
+++ .emacs.d/settings/global-settings.el
@@ -2,16 +2,6 @@
 ;;; Global Settings ;;;
 ;---------------------;
 
-; set PATH, because we don't load .bashrc
-; function from https://gist.github.com/jakemcc/3887459
-(defun set-exec-path-from-shell-PATH ()
-  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo -n $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
- 
-(if window-system (set-exec-path-from-shell-PATH))
-
 ; language
 (setq current-language-environment "English")
 
@@ -75,6 +65,8 @@
 (global-hi-lock-mode nil)
 (setq jit-lock-contextually 1)
 (setq jit-lock-stealth-verbose 1)
+
+(set-default-font "DejaVu Sans Mono-9")
 
 ; if there is size information associated with text, change the text
 ; size to reflect it

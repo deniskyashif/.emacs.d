3ee7790305214a920da2f330f9842744ed83c9d4
deniskyashif <denis.kyashif@gmail.com>
2016-02-13 23:35:21 +0200

    updated dotfiles and company-settings.el

--- .emacs.d/settings/company-settings.el
+++ .emacs.d/settings/company-settings.el
@@ -1,9 +1,15 @@
 (require 'company)
+(require 'company-web-html)                          ; load company mode html backend
 
-(setq company-idle-delay 0.5)
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-tooltip-align-annotations 't)          ; align annotations to the right tooltip border
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
 (setq company-tooltip-limit 10)
 (setq company-minimum-prefix-length 2)
 (setq company-tooltip-flip-when-above t)
+
+(global-set-key (kbd "C-c /") 'company-files)        ; Force complete file names on "C-c /" key
 
 (global-company-mode 1)
 

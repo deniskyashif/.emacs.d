17e126d864f2093ddafbc772ed99efa527ffd670
deniskyashif <denis.kyashif@gmail.com>
2017-10-22 09:33:08 +0300

    disabled tern mode for js

--- settings/js-settings.el
+++ settings/js-settings.el
@@ -4,13 +4,13 @@
 
 (require 'flycheck)
 (require 'web-beautify)
-(require 'company-tern)
+;; (require 'company-tern)
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
 (add-to-list 'auto-mode-alist '("^\.tern-project$" . json-mode))
 
-(add-to-list 'company-backends 'company-tern)
+;; (add-to-list 'company-backends 'company-tern)
 
 (setq js2-highlight-level 3)
 (setq-default js2-basic-offset 4)
@@ -38,9 +38,9 @@
 			   (linum-mode 1)))
 
 ;; fix error when tern does not autorefresh (https://truongtx.me/2014/04/20/emacs-javascript-completion-and-refactoring/)
-(defun delete-tern-process ()
-  (interactive)
-  (delete-process "Tern"))
+;; (defun delete-tern-process ()
+;;   (interactive)
+;;   (delete-process "Tern"))
 
 (provide 'js-settings)
 
--- settings/web-mode-settings.el
+++ settings/web-mode-settings.el
@@ -40,15 +40,15 @@
       (flycheck-mode))))
 
 ;; Enable JavaScript completion between <script>...</script> etc.
-(defadvice company-tern (before web-mode-set-up-ac-sources activate)
-  "Set `tern-mode' based on current language before running company-tern."
-  (if (equal major-mode 'web-mode)
-      (let ((web-mode-cur-language
-             (web-mode-language-at-pos)))
-        (if (or (string= web-mode-cur-language "javascript")
-               (string= web-mode-cur-language "jsx"))
-            (unless tern-mode (tern-mode))
-          (if tern-mode (tern-mode))))))
+;; (defadvice company-tern (before web-mode-set-up-ac-sources activate)
+;;   "Set `tern-mode' based on current language before running company-tern."
+;;   (if (equal major-mode 'web-mode)
+;;       (let ((web-mode-cur-language
+;;              (web-mode-language-at-pos)))
+;;         (if (or (string= web-mode-cur-language "javascript")
+;;                (string= web-mode-cur-language "jsx"))
+;;             (unless tern-mode (tern-mode))
+;;           (if tern-mode (tern-mode))))))
 
 (add-hook 'web-mode-hook 'emmet-mode)
 (add-hook 'web-mode-hook 'my:web-mode-hook)
